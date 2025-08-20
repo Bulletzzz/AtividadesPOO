18ff8d0a577f95f161476caff5e4ab8e35c4e7c8
Bernardo Kuster <Bernardokrc@gmail.com>
2025-08-20 15:34:44 -0300

    segundo exercicio

---
+++ atividade2/ex2/Main.java
@@ -0,0 +1,11 @@
+package atividade2.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculadora calculo = new Calculadora();
+
+        System.out.println(calculo.soma(12, 5));
+        System.out.println(calculo.soma(1.6, 2.4));
+        System.out.println(calculo.soma(2, 2, 8));
+    }
+}
